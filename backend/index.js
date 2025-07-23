18762f184735ae21da137aecfa80797b017e37b5
gulshan <gulshanjangid333@gmail.com>
2025-07-23 15:14:59 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 // app.use('/uploads', express.static('uploads')); // Serve uploaded images
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-
+ 
 // Initialize Socket.IO
 const server = require('http').createServer(app);
 const io = socketIo(server, {
